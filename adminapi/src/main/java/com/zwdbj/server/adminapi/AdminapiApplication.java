5f77686163d07569392e98f0c16331f92c0e6e06
limingru <limingru@zwdbj.com>
2018-10-27 17:21:35 +0800

    add api

---
+++ adminapi/src/main/java/com/zwdbj/server/adminapi/AdminapiApplication.java
@@ -0,0 +1,12 @@
+package com.zwdbj.server.adminapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminapiApplication.class, args);
+    }
+}
---
+++ mobileapi/src/main/java/com/zwdbj/server/mobileapi/MobileapiApplication.java
@@ -0,0 +1,12 @@
+package com.zwdbj.server.mobileapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileapiApplication.class, args);
+    }
+}
