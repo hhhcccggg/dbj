519e3a1bd1e3fabe074d7cac70657a5ac601ef48
wangpeihan <wangpeihan@zwdbj.com>
2018-11-01 16:07:30 +0800

    ok

--- adminserver/src/main/java/com/zwdbj/server/adminserver/service/video/mapper/VideoSqlProvider.java
+++ adminserver/src/main/java/com/zwdbj/server/adminserver/service/video/mapper/VideoSqlProvider.java
@@ -95,7 +95,7 @@ public class VideoSqlProvider {
         SQL sql = new SQL()
                 .SELECT("*")
                 .FROM("core_videos")
-                .WHERE("complainCount!=0");
+                .WHERE("complainCount!=0 and status=0");
         if (model.getKeyword()!=null && model.getKeyword().length()>0){
             sql.WHERE(String.format("title like '%s'",("%"+model.getKeyword()+"%")));
         }
@@ -143,25 +143,6 @@ public class VideoSqlProvider {
         return sql.toString();
     }
 
-   /* public String nearby(Map params) {
-        Double longitude = (Double) params.get("longitude");
-        Double latitude = (Double) params.get("latitude");
-        float distance = (float) params.get("distance");
-
-        double[] results = VideoSqlProvider.getAround(latitude,longitude,distance);
-
-        SQL sql = new SQL()
-                .SELECT("*")
-                .FROM("core_videos")
-                .WHERE(String.format("longitude BETWEEN %f AND %f",results[1],results[3]))
-                .AND()
-                .WHERE(String.format("latitude  BETWEEN %f AND %f",results[0],results[2]))
-                .AND()
-                .WHERE("status=0")
-                .ORDER_BY("heartCount desc");
-        return sql.toString();
-    }
-*/
     public String updateVideoField(Map params) {
         Long id = (Long)params.get("id");
         String fields = (String)params.get("fields");
@@ -172,27 +153,4 @@ public class VideoSqlProvider {
         return sql.toString();
     }
 
-
-
-
-   /* public static double[] getAround(double lat,double lon,float radius) {
-
-        Double latitude = lat;
-        Double longitude = lon;
-
-        Double degree = (24901 * 1609) / 360.0;
-        double radiusMile = radius;
-
-        Double dpmLat = 1 / degree;
-        Double radiusLat = dpmLat * radiusMile;
-        Double minLat = latitude - radiusLat;
-        Double maxLat = latitude + radiusLat;
-
-        Double mpdLng = degree * Math.cos(latitude * (PI / 180));
-        Double dpmLng = 1 / mpdLng;
-        Double radiusLng = dpmLng * radiusMile;
-        Double minLng = longitude - radiusLng;
-        Double maxLng = longitude + radiusLng;
-        return new double[]{minLat, minLng, maxLat, maxLng};
-    }*/
 }
