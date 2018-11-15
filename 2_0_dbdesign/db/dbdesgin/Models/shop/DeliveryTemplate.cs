f5db7a13a0541ef0c7f0c9b4b2bfc573eb74f931
limingru <limingru@zwdbj.com>
2018-11-15 16:03:13 +0800

    数据结构

---
+++ 2_0_dbdesign/db/dbdesgin/Models/shop/DeliveryTemplate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace db.video.Models.shop
+{
+    /// <summary>
+    /// 物流模板
+    /// </summary>
+    [Table("shop_deliveryTemplates")]
+    public class DeliveryTemplate : db.common.BaseModelWithTime<long>
+    {
+        [Required]
+        [MaxLength(30)]
+        public string name { get; set; }
+        /// <summary>
+        /// 计费类型0:计件1：重量kg
+        /// </summary>
+        /// <value>The type of the bill.</value>
+        public int billType { get; set; }
+    }
+}
