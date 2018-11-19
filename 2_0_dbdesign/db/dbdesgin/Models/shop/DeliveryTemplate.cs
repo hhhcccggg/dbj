2a046b04d071875ec3a6599af020886f3b335c62
limingru <limingru@zwdbj.com>
2018-11-19 13:27:41 +0800

    解决冲突

--- 2_0_dbdesign/db/dbdesgin/Models/shop/DeliveryTemplate.cs
+++ 2_0_dbdesign/db/dbdesgin/Models/shop/DeliveryTemplate.cs
@@ -10,14 +10,11 @@ namespace db.video.Models.shop
     [Table("shop_deliveryTemplates")]
     public class DeliveryTemplate : db.common.BaseModelWithTime<long>
     {
-<<<<<<< HEAD
         /// <summary>
         /// 卖家ID
         /// </summary>
         /// <value>The seller identifier.</value>
         public long sellerId { get; set; }
-=======
->>>>>>> f5db7a13a0541ef0c7f0c9b4b2bfc573eb74f931
         [Required]
         [MaxLength(30)]
         public string name { get; set; }
@@ -26,13 +23,10 @@ namespace db.video.Models.shop
         /// </summary>
         /// <value>The type of the bill.</value>
         public int billType { get; set; }
-<<<<<<< HEAD
         /// <summary>
         /// 配送范围
         /// </summary>
         /// <value>The delivery template scope identifier.</value>
         public long deliveryTemplateScopeId { get; set; }
-=======
->>>>>>> f5db7a13a0541ef0c7f0c9b4b2bfc573eb74f931
     }
 }
