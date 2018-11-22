734bdbf79297aa5101025c39f384295708dec422
wangpeihan <wangpeihan@zwdbj.com>
2018-11-22 15:35:27 +0800

    增加后台数据操作：修改用户的视频标题

--- adminoperate/src/main/java/com/zwdbj/server/quartz/quartzConfig/MyScheduler.java
+++ adminoperate/src/main/java/com/zwdbj/server/quartz/quartzConfig/MyScheduler.java
@@ -19,13 +19,13 @@ public class MyScheduler {
         StdScheduler stdScheduler = (StdScheduler) annotationContext.getBean("mySchedulerFactoryBean2");//获得上面创建的bean
         Scheduler myScheduler =stdScheduler;
         JobKey jobKey1  = new JobKey("job1", "group02");
-        JobKey jobKey8  = new JobKey("job2", "group02");
-        JobKey jobKey7  = new JobKey("job7", "group02");
-        JobKey jobKey9  = new JobKey("job9", "group02");
+        JobKey jobKey2  = new JobKey("job2", "group02");
+        JobKey jobKey3  = new JobKey("job3", "group02");
+        JobKey jobKey4  = new JobKey("job4", "group02");
         if (!myScheduler.checkExists(jobKey1)) startJob1(myScheduler);
-        if (!myScheduler.checkExists(jobKey8)) startJob8(myScheduler);
-        if (!myScheduler.checkExists(jobKey7)) startJob7(myScheduler);
-        if (!myScheduler.checkExists(jobKey9)) startJob9(myScheduler);
+        if (!myScheduler.checkExists(jobKey2)) startJob2(myScheduler);
+        if (!myScheduler.checkExists(jobKey3)) startJob3(myScheduler);
+        if (!myScheduler.checkExists(jobKey4)) startJob4(myScheduler);
         myScheduler.start();
 
     }
@@ -42,35 +42,35 @@ public class MyScheduler {
     }
 
 
-    private void startJob8(Scheduler scheduler) throws SchedulerException{
+    private void startJob2(Scheduler scheduler) throws SchedulerException{
         JobDetail jobDetail = JobBuilder.newJob(GreatVestUser2Job.class)
                 .withIdentity("job2", "group02")
                 .build();
-        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule("0 25 5 * * ?");
+        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule("0 0 3 * * ?");
         CronTrigger cronTrigger = TriggerBuilder.newTrigger()
                 .withIdentity("trigger2", "group2")
                 .withSchedule(scheduleBuilder)
                 .build();
         scheduler.scheduleJob(jobDetail,cronTrigger);
     }
-    private void startJob7(Scheduler scheduler) throws SchedulerException{
+    private void startJob3(Scheduler scheduler) throws SchedulerException{
         JobDetail jobDetail = JobBuilder.newJob(MyFollowersJob.class)
-                .withIdentity("job7", "group02")
+                .withIdentity("job3", "group02")
                 .build();
-        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule("0 5 6 * * ?");
+        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule("0 15 3 * * ?");
         CronTrigger cronTrigger = TriggerBuilder.newTrigger()
-                .withIdentity("trigger7", "group2")
+                .withIdentity("trigger3", "group2")
                 .withSchedule(scheduleBuilder)
                 .build();
         scheduler.scheduleJob(jobDetail,cronTrigger);
     }
-    private void startJob9(Scheduler scheduler) throws SchedulerException{
+    private void startJob4(Scheduler scheduler) throws SchedulerException{
         JobDetail jobDetail = JobBuilder.newJob(VideosToUsersJob.class)
-                .withIdentity("job9", "group02")
+                .withIdentity("job4", "group02")
                 .build();
-        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule("0 5 7 * * ?");
+        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule("0 5 6 * * ?");
         CronTrigger cronTrigger = TriggerBuilder.newTrigger()
-                .withIdentity("trigger9", "group2")
+                .withIdentity("trigger4", "group2")
                 .withSchedule(scheduleBuilder)
                 .build();
         scheduler.scheduleJob(jobDetail,cronTrigger);
--- adminserver/src/main/java/com/zwdbj/server/adminserver/service/quartzService/QuartzService.java
+++ adminserver/src/main/java/com/zwdbj/server/adminserver/service/quartzService/QuartzService.java
@@ -24,8 +24,6 @@ import java.util.List;
 @Service
 public class QuartzService {
     @Autowired
-    private StringRedisTemplate stringRedisTemplate;
-    @Autowired
     UserService userService;
     @Autowired
     VideoService videoService;
@@ -118,69 +116,4 @@ public class QuartzService {
         }
     }
 
-    /**
-     * 每天增加马甲用户
-     */
-    /*public void greatVestUser(){
-        try {
-            for (int i = 1; i <= 50; i++) {
-                if (i%5==0){
-                    this.operateService.newVestUser1();
-                    logger.info("我是默认马甲"+new SimpleDateFormat("HH:mm:ss").format(new Date()));
-                }else if (i%5==2){
-                    this.operateService.newVestUser2(2);
-                    logger.info("我是不同的马甲2: "+new SimpleDateFormat("HH:mm:ss").format(new Date()));
-                }else if (i%5==3 || i%5==1){
-                    this.operateService.newVestUser2(3);
-                    logger.info("我是不同的马甲3: "+new SimpleDateFormat("HH:mm:ss").format(new Date()));
-                }else if (i%5==4){
-                    this.operateService.newVestUser2(4);
-                    logger.info("我是不同的马甲4: "+new SimpleDateFormat("HH:mm:ss").format(new Date()));
-                }
-            }
-        }catch (Exception e){
-            logger.info("增加用户异常"+e.getMessage());
-        }
-    }*/
-
-    /**
-     * 定时增加视频的播放量和点赞量
-     */
-    /*public void increaseHeartAndPlayCount() {
-        try {
-            logger.info("定时增加视频的播放量和点赞量++++++" + new SimpleDateFormat("HH:mm:ss").format(new Date()));
-            List<VideoHeartAndPlayCountDto> videoHeartAndPlayCountDtos = this.videoService.findHeartAndPlayCount();
-            if (videoHeartAndPlayCountDtos == null) return;
-            int count =  new Double(Math.ceil(videoHeartAndPlayCountDtos.size()*0.8)).intValue();
-            for (int j=0; j<count; j++) {
-                VideoHeartAndPlayCountDto dto = videoHeartAndPlayCountDtos.get(this.operateService.getRandom(0,videoHeartAndPlayCountDtos.size()));
-                int dianzhan = this.operateService.getRandom(20, 37);
-                int pinlun = this.operateService.getRandom(4, 7);
-                int fenxiang = this.operateService.getRandom(1, 3);
-                int addPlayCount = this.operateService.getRandom(50, 201);
-                this.videoService.updateField("playCount=playCount+" + addPlayCount, dto.getId());
-                this.videoService.updateField("heartCount=heartCount+" + new Double(Math.ceil(addPlayCount * dianzhan / 100.0)).longValue(), dto.getId());
-                Long addHeartCount = this.videoService.findVideoHeartCount(dto.getId()) - dto.getHeartCount();
-                this.userService.updateField("totalHearts=totalHearts+" + addHeartCount, dto.getUserId());
-                this.videoService.updateField("shareCount=shareCount+" + new Double(Math.ceil(addHeartCount * fenxiang / 100.0)).longValue(), dto.getId());
-                int comment = (int) Math.ceil(addHeartCount * pinlun / 100.0);
-                String redisComment =  this.stringRedisTemplate.opsForValue().get("REDIS_COMMENTS");
-                String[] redisComments = redisComment.split(">");
-                int size = redisComments.length;
-                if (dto.getCommentCount()>=size)comment=0;
-                int tem = 0;
-                for (int i = 0; i < comment; i++) {
-                    int gg = this.operateService.commentVideo1(dto.getId());
-                    if (gg==0)tem--;
-                }
-                comment = comment + tem;
-                if (comment==0)return;
-                this.videoService.updateField("commentCount=commentCount+" + comment, dto.getId());
-                logger.info("播放量不超过8000=++++++" + new SimpleDateFormat("HH:mm:ss").format(new Date()));
-            }
-            }catch(Exception e){
-                logger.info("increaseHeartAndPlayCount异常" + e.getMessage());
-            }
-
-        }*/
     }
