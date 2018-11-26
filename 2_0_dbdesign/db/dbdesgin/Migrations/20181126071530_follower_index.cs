07d36273218131955442d62e7a7d204c92407ecb
limingru <limingru@zwdbj.com>
2018-11-26 15:21:03 +0800

    add index for core_userFollowers

---
+++ 2_0_dbdesign/db/dbdesgin/Migrations/20181126071530_follower_index.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dbdesgin.Migrations
+{
+    public partial class follower_index : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_core_followers_followerUserId",
+                table: "core_followers",
+                column: "followerUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_core_followers_userId",
+                table: "core_followers",
+                column: "userId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_core_followers_followerUserId",
+                table: "core_followers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_core_followers_userId",
+                table: "core_followers");
+        }
+    }
+}
