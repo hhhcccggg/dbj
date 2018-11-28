46b643cdcdd6d209b60f5746527cc0493997d84a
hhhcccggg <18380212720@163.com>
2018-11-28 11:36:04 +0800

    创建实体类

---
+++ shop/shop-service/src/main/java/com/zwdbj/server/shopservice/product/service/ProductServiceImpl.java
@@ -0,0 +1,9 @@
+package com.zwdbj.server.shopservice.product.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+}
