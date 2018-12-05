e9c2741cbafdc3a77f79b5448d6dd88736d1ada0
limingru <limingru@zwdbj.com>
2018-12-05 16:27:50 +0800

    增加打赏相关结构

---
+++ 2_0_dbdesign/db/dbdesgin/Migrations/20181205082407_tip_table.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dbdesgin.Migrations
+{
+    public partial class tip_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "tipCount",
+                table: "core_videos",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateTable(
+                name: "core_basic_buyCoinConfigs",
+                columns: table => new
+                {
+                    id = table.Column<long>(maxLength: 128, nullable: false),
+                    createTime = table.Column<DateTime>(type: "timestamp", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP()"),
+                    isDeleted = table.Column<bool>(nullable: false, defaultValue: false),
+                    deleteTime = table.Column<DateTime>(type: "timestamp", nullable: true),
+                    isManualData = table.Column<bool>(nullable: false, defaultValue: false),
+                    coins = table.Column<int>(nullable: false),
+                    rmbs = table.Column<int>(nullable: false),
+                    title = table.Column<string>(maxLength: 30, nullable: false),
+                    orderIndex = table.Column<int>(nullable: false, defaultValue: 0)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_core_basic_buyCoinConfigs", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "core_video_videoTipDetails",
+                columns: table => new
+                {
+                    id = table.Column<long>(maxLength: 128, nullable: false),
+                    createTime = table.Column<DateTime>(type: "timestamp", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP()"),
+                    isDeleted = table.Column<bool>(nullable: false, defaultValue: false),
+                    deleteTime = table.Column<DateTime>(type: "timestamp", nullable: true),
+                    isManualData = table.Column<bool>(nullable: false, defaultValue: false),
+                    videoId = table.Column<long>(nullable: false),
+                    userId = table.Column<long>(nullable: false),
+                    tipCoin = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_core_video_videoTipDetails", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_core_video_videoTipDetails_userId",
+                table: "core_video_videoTipDetails",
+                column: "userId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_core_video_videoTipDetails_videoId",
+                table: "core_video_videoTipDetails",
+                column: "videoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "core_basic_buyCoinConfigs");
+
+            migrationBuilder.DropTable(
+                name: "core_video_videoTipDetails");
+
+            migrationBuilder.DropColumn(
+                name: "tipCount",
+                table: "core_videos");
+        }
+    }
+}
---
+++ 2_0_dbdesign/db/dbdesgin/Models/VideoTipDetail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dbdesgin.Models
+{
+    /// <summary>
+    /// 视频打赏详情
+    /// </summary>
+    [Table("core_video_videoTipDetails")]
+    public class VideoTipDetail : db.common.BaseModelWithTime<long>
+    {
+        /// <summary>
+        /// 被打赏的视频
+        /// </summary>
+        /// <value>The video identifier.</value>
+        public long videoId { get; set; }
+        /// <summary>
+        /// 打赏视频的用户
+        /// </summary>
+        /// <value>The user identifier.</value>
+        public long userId { get; set; }
+        /// <summary>
+        /// 打赏的金币
+        /// </summary>
+        /// <value>The tip coin.</value>
+        public long tipCoin { get; set; }
+    }
+}
