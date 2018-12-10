1a2f0c3031790be30d2314dd3e6b3a7f4c32a35b
hhhcccggg <18380212720@163.com>
2018-12-10 10:08:41 +0800

    Merge branch 'hechenggang' of http://git.zwdbj.com:3000/petserver/petserverapp into hechenggang
                                                                                                       # Conflicts:
                                                                                                       #	shop/shop-service/src/main/java/com/zwdbj/server/shopservice/product/mapper/IProductMapper.java

--- adminserver/src/main/java/com/zwdbj/server/adminserver/controller/UserAssetsController.java
+++ adminserver/src/main/java/com/zwdbj/server/adminserver/controller/UserAssetsController.java
@@ -4,6 +4,8 @@ package com.zwdbj.server.adminserver.controller;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.zwdbj.server.adminserver.service.userAssets.model.UserAssets;
+import com.zwdbj.server.adminserver.service.userAssets.model.UserCoinDetail;
+import com.zwdbj.server.adminserver.service.userAssets.model.UserCoinType;
 import com.zwdbj.server.adminserver.service.userAssets.service.UserAssetsService;
 import com.zwdbj.server.utility.model.ResponseData;
 import com.zwdbj.server.utility.model.ResponseDataCode;
@@ -24,20 +26,63 @@ public class UserAssetsController {
     private UserAssetsService userAssetsServiceImpl;
 
     @ApiOperation(value = "查询所有用户资产相关")
-    @RequestMapping(value = "/search", method = RequestMethod.GET)
-    public ResponsePageInfoData<List<UserAssets>> searchAll(@RequestParam(value = "pageNo", defaultValue = "1", required = true) int pageNo,
-                                                            @RequestParam(value = "rows", defaultValue = "30", required = true) int rows) {
+    @RequestMapping(value = "/searchUserAssets", method = RequestMethod.GET)
+    public ResponsePageInfoData<List<UserAssets>> searchAllUserAssets(@RequestParam(value = "pageNo", defaultValue = "1", required = true) int pageNo,
+                                                                      @RequestParam(value = "rows", defaultValue = "30", required = true) int rows) {
 
         PageHelper.startPage(pageNo, rows);
-        List<UserAssets> result = this.userAssetsServiceImpl.searchAll().getData();
+        List<UserAssets> result = this.userAssetsServiceImpl.searchAllUserAssets().getData();
         PageInfo pageInfo = new PageInfo(result);
         return new ResponsePageInfoData<>(ResponseDataCode.STATUS_NORMAL, "", result, pageInfo.getTotal());
     }
 
-    @RequestMapping(value = "/search/{userId}",method = RequestMethod.GET)
+    @RequestMapping(value = "/searchUserAssets/{userId}", method = RequestMethod.GET)
     @ApiOperation(value = "通过id查询用户资产")
-    public ResponseData<UserAssets> searchByUserId(@PathVariable("userId") Long userId) {
-        ServiceStatusInfo<UserAssets> serviceStatusInfo = this.userAssetsServiceImpl.searchByUserId(userId);
+    public ResponseData<UserAssets> searchUserAssetsByUserId(@PathVariable("userId") Long userId) {
+        ServiceStatusInfo<UserAssets> serviceStatusInfo = this.userAssetsServiceImpl.searchUserAssetsByUserId(userId);
+        if (serviceStatusInfo.isSuccess()) {
+            return new ResponseData<>(ResponseDataCode.STATUS_NORMAL, "", serviceStatusInfo.getData());
+        }
+        return new ResponseData<>(ResponseDataCode.STATUS_ERROR, serviceStatusInfo.getMsg(), null);
+    }
+
+    @ApiOperation(value = "分类查询用户金币总额")
+    @RequestMapping(value = "/searchUserCoinType", method = RequestMethod.GET)
+    public ResponsePageInfoData<List<UserCoinType>> searchAllUserCoinType(@RequestParam(value = "pageNo", defaultValue = "1", required = true) int pageNo,
+                                                                          @RequestParam(value = "rows", defaultValue = "30", required = true) int rows) {
+
+        PageHelper.startPage(pageNo, rows);
+        List<UserCoinType> result = this.userAssetsServiceImpl.searchAllUserCoinTyoe().getData();
+        PageInfo pageInfo = new PageInfo(result);
+        System.out.println(result);
+        return new ResponsePageInfoData<>(ResponseDataCode.STATUS_NORMAL, "", result, pageInfo.getTotal());
+    }
+
+    @RequestMapping(value = "/searchUserCoinType/{userId}", method = RequestMethod.GET)
+    @ApiOperation(value = "通过id分类查询用户金币总额")
+    public ResponseData<UserCoinType> searchUserCoinTypeByUserId(@PathVariable("userId") Long userId) {
+        ServiceStatusInfo<UserCoinType> serviceStatusInfo = this.userAssetsServiceImpl.searchUserCoinTypeByUserId(userId);
+        if (serviceStatusInfo.isSuccess()) {
+            return new ResponseData<>(ResponseDataCode.STATUS_NORMAL, "", serviceStatusInfo.getData());
+        }
+        return new ResponseData<>(ResponseDataCode.STATUS_ERROR, serviceStatusInfo.getMsg(), null);
+    }
+
+    @ApiOperation(value = "查询所有用户金币明细")
+    @RequestMapping(value = "/searchUserCoinDetail", method = RequestMethod.GET)
+    public ResponsePageInfoData<List<UserCoinDetail>> searchAllUserCoinDetail(@RequestParam(value = "pageNo", defaultValue = "1", required = true) int pageNo,
+                                                                              @RequestParam(value = "rows", defaultValue = "30", required = true) int rows) {
+
+        PageHelper.startPage(pageNo, rows);
+        List<UserCoinDetail> result = this.userAssetsServiceImpl.searchAllUserCoinDetail().getData();
+        PageInfo pageInfo = new PageInfo(result);
+        return new ResponsePageInfoData<>(ResponseDataCode.STATUS_NORMAL, "", result, pageInfo.getTotal());
+    }
+
+    @RequestMapping(value = "/searchUserCoinDetail/{userId}", method = RequestMethod.GET)
+    @ApiOperation(value = "通过id查询用户资产")
+    public ResponseData<UserCoinDetail> searchByUserId(@PathVariable("userId") Long userId) {
+        ServiceStatusInfo<UserCoinDetail> serviceStatusInfo = this.userAssetsServiceImpl.searchUserCoinDetailByUserId(userId);
         if (serviceStatusInfo.isSuccess()) {
             return new ResponseData<>(ResponseDataCode.STATUS_NORMAL, "", serviceStatusInfo.getData());
         }
--- adminserver/src/main/java/com/zwdbj/server/adminserver/service/userAssets/mapper/UserAssetsMapper.java
+++ adminserver/src/main/java/com/zwdbj/server/adminserver/service/userAssets/mapper/UserAssetsMapper.java
@@ -2,6 +2,8 @@ package com.zwdbj.server.adminserver.service.userAssets.mapper;
 
 
 import com.zwdbj.server.adminserver.service.userAssets.model.UserAssets;
+import com.zwdbj.server.adminserver.service.userAssets.model.UserCoinDetail;
+import com.zwdbj.server.adminserver.service.userAssets.model.UserCoinType;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -11,9 +13,21 @@ import java.util.List;
 @Mapper
 public interface UserAssetsMapper {
     @Select("select *  from core_userAssets where userId=#{userId} ")
-    UserAssets searchByUserId(@Param("userId") Long userId);
+    UserAssets searchUserAssetsByUserId(@Param("userId") Long userId);
 
     @Select("select * from core_userAssets ")
-    List<UserAssets> searchAll();
+    List<UserAssets> searchAllUserAssets();
 
+    @Select("select * from core_userCoinDetails ")
+    List<UserCoinDetail> searchAllUserCoinDetail();
+
+    @Select("select * from core_userCoinDetails where userId=#{userId}")
+    UserCoinDetail searchUserCoinDetailByUserId(@Param("userId") Long userId);
+
+
+    @Select("select * from core_userCoinTypes")
+    List<UserCoinType> searchAllUserCoinTypes();
+
+    @Select("select * from core_userCoinTypes where userId=#{userId}")
+    UserCoinType searchUserCoinTpyesByUserId(@Param("userId") Long userId);
 }
