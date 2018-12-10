a0a285e7a251e50dc4b59e4d3d76b189f1928ae5
hhhcccggg <18380212720@163.com>
2018-12-10 17:12:50 +0800

    Merge branch 'hechenggang' of http://git.zwdbj.com:3000/petserver/petserverapp into hechenggang
                                                                                                       # Conflicts:
                                                                                                       #	shop/shop-service/src/main/java/com/zwdbj/server/shopservice/product/mapper/IProductMapper.java

---
+++ adminserver/src/main/java/com/zwdbj/server/adminserver/purchase/model/Receipt.java
@@ -0,0 +1,35 @@
+package com.zwdbj.server.adminserver.purchase.model;
+
+//服务器返回的验证信息
+public class Receipt {
+
+    private String original_purchase_date_pst; //购买时间,太平洋标准时间
+
+    private String purchase_date_ms; //购买时间毫秒
+
+    private String unique_identifier;//唯一标识符
+
+    private String original_transaction_id;//原始交易ID
+
+    private String bvrs;//iPhone程序的版本号
+
+    private String transaction_id; //交易的标识
+
+    private String quantity;
+
+    private String unique_vendor_identifier; //开发商交易ID
+
+    private String item_id;
+
+    private String product_id;//商品的标识
+
+    private String purchase_date;//购买时间
+
+    private String original_purchase_date; //原始购买时间
+
+    private String purchase_date_pst;//太平洋标准时间
+
+    private String bid;//iPhone程序的bundle标识
+
+    private String original_purchase_date_ms;//毫秒
+}
