fa3b1b47da865271a0e69cbeaa4e41301e33b75a
hhhcccggg <18380212720@163.com>
2018-12-10 10:09:02 +0800

    Merge branch 'hechenggang' of http://git.zwdbj.com:3000/petserver/petserverapp into hechenggang
                                                                                                       # Conflicts:
                                                                                                       #	shop/shop-service/src/main/java/com/zwdbj/server/shopservice/product/mapper/IProductMapper.java

---
+++ adminserver/src/main/java/com/zwdbj/server/adminserver/service/userAssets/model/UserCoinDetail.java
@@ -0,0 +1,90 @@
+package com.zwdbj.server.adminserver.service.userAssets.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "用户金币明细")
+public class UserCoinDetail {
+    @ApiModelProperty(value = "id")
+    Long id;
+    @ApiModelProperty(value = "金币数")
+    Integer num;
+    @ApiModelProperty(value = "标题")
+    String title;
+    @ApiModelProperty(value = "附加数据")
+    String extraData;
+
+    @ApiModelProperty(value = "获得渠道")
+    String type;
+
+    @ApiModelProperty(value = "用户Id")
+    Long userId;
+    @ApiModelProperty(value = "状态")
+    String status;
+    @ApiModelProperty(value = "状态原因说明")
+    String statusMsg;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getExtraData() {
+        return extraData;
+    }
+
+    public void setExtraData(String extraData) {
+        this.extraData = extraData;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatusMsg() {
+        return statusMsg;
+    }
+
+    public void setStatusMsg(String statusMsg) {
+        this.statusMsg = statusMsg;
+    }
+}
