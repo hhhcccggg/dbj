a43ea66294d0875e4fca7b7ae0d9bf60630edfe6
wangpeihan <wangpeihan@zwdbj.com>
2018-12-11 13:15:34 +0800

    根据tag查询video

--- mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/video/mapper/IVideoMapper.java
+++ mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/video/mapper/IVideoMapper.java
@@ -37,11 +37,11 @@ public interface IVideoMapper {
     List<VideoInfoDto> listHot(@Param("conditionValue") String conditionValue);
     @Select("select * from core_videos where status=0 order by createTime desc")
     List<VideoInfoDto> listLatest();
-    @Select("select * from core_videos where tags=#{tags} order by heartCount desc")
+    @Select("select * from core_videos where tags like concat('%',#{tags},'%') order by heartCount desc")
     List<VideoInfoDto> listByTag(@Param("tags")String tag);
-    @Select("select * from core_videos where tags=#{tags} order by heartCount desc")
+    @Select("select * from core_videos where tags like concat('%',#{tags},'%') order by heartCount desc")
     List<VideoInfoDto> listByTagName1(@Param("tags")String tagName);
-    @Select("select * from core_videos where tags=#{tags} order by createTime desc")
+    @Select("select * from core_videos where tags like concat('%',#{tags},'%') order by createTime desc")
     List<VideoInfoDto> listByTagName2(@Param("tags")String tagName);
     @Select("select * from core_videos where id=#{id}")
     VideoDetailInfoDto video(@Param("id") long id);
