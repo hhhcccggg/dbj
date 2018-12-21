d670c05ac4873812087898b7c599bad66c2a8d97
limingru <limingru@zwdbj.com>
2018-12-21 16:44:51 +0800

    兼容处理

--- mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/video/service/VideoService.java
+++ mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/video/service/VideoService.java
@@ -146,10 +146,10 @@ public class VideoService {
         return videoInfoDtos;
     }
 
-    public List<VideoInfoDto> listHot(Page<VideoInfoDto> pageInfo) {
+    public List<VideoInfoDto> listHot(Page<VideoInfoDto> pageInfo,int pageSize) {
         long userId = JWTUtil.getCurrentId();
         if (userId>0) {
-            List<Long> videoIDS = this.videoRandRecommendService.fetchVideo("u_"+String.valueOf(userId),pageInfo.getPageSize());
+            List<Long> videoIDS = this.videoRandRecommendService.fetchVideo("u_"+String.valueOf(userId),pageSize);
             if(videoIDS.size()==0) return new ArrayList<>();
             List<VideoInfoDto> recommendVideos = this.videoMapper.listIds(StringUtils.join(videoIDS.toArray(),","));
             if (recommendVideos!=null) {
