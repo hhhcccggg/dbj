1af509ed0b3dfd92a993e69abc3158a65165af21
wangpeihan <wangpeihan@zwdbj.com>
2018-12-21 11:16:38 +0800

    iosPurchase

--- mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/pay/wechat/service/WXPayService.java
+++ mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/pay/wechat/service/WXPayService.java
@@ -54,6 +54,8 @@ public class WXPayService {
         detailInput.setNum(input.getCoins());
         detailInput.setExtraData("");
         detailInput.setType("PAY");
+        detailInput.setTradeNo("");
+        detailInput.setTradeType("WECHAT");
         long id = this.userAssetServiceImpl.addUserCoinDetail(userId,detailInput);
         // 生成预付单
         UnifiedOrderInput unifiedOrderInput = new UnifiedOrderInput();
@@ -121,6 +123,7 @@ public class WXPayService {
             coinDetailModifyInput.setId(Long.parseLong(resultDto.getOutTradeNo()));
             coinDetailModifyInput.setType("PAY");
             coinDetailModifyInput.setStatus("SUCCESS");
+            coinDetailModifyInput.setTradeNo(resultDto.getTransactionId());
             this.userAssetServiceImpl.updateUserCoinDetail(coinDetailModifyInput);
         } else {
             logger.info("交易失败");
