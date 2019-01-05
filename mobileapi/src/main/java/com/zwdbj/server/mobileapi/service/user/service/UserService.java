9bbec9a98e4c617e11e7d6456b86ed8eb24d7e34
wangpeihan <wangpeihan@zwdbj.com>
2019-01-05 16:15:25 +0800

    密码正则

--- mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/user/service/UserService.java
+++ mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/user/service/UserService.java
@@ -677,7 +677,7 @@ public class UserService {
                     return  new ServiceStatusInfo<>(0,"",100);
                 }
             }else {
-                new ResponseData<>(ResponseDataCode.STATUS_ERROR, "验证失败，请输入正确的手机号和验证码", null);
+                new ResponseData<>(1, "验证失败，请输入正确的手机号和验证码", null);
             }
 
         }catch (Exception e){
