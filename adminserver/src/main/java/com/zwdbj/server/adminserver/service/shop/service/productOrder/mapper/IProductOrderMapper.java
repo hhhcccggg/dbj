e5ee499fe15e72671b1c624e201e1c48a16b27cc
wangpeihan <wangpeihan@zwdbj.com>
2019-01-08 18:08:08 +0800

    admin-shop-order

--- adminserver/src/main/java/com/zwdbj/server/adminserver/service/shop/service/productOrder/mapper/IProductOrderMapper.java
+++ adminserver/src/main/java/com/zwdbj/server/adminserver/service/shop/service/productOrder/mapper/IProductOrderMapper.java
@@ -1,7 +1,21 @@
 package com.zwdbj.server.adminserver.service.shop.service.productOrder.mapper;
 
+import com.zwdbj.server.adminserver.service.shop.service.productOrder.model.ProductOrderDetailModel;
+import com.zwdbj.server.adminserver.service.shop.service.productOrder.model.ProductOrderInput;
+import com.zwdbj.server.adminserver.service.shop.service.productOrder.model.ProductOrderModel;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectProvider;
+
+import java.util.List;
 
 @Mapper
 public interface IProductOrderMapper {
+
+    @SelectProvider(type = ProductOrderSqlProvider.class,method = "getStoreOrders")
+    List<ProductOrderModel> getStoreOrders(@Param("storeId")long storeId, @Param("input")ProductOrderInput input);
+    @Select("select o.*,oi.productId,oi.productskuId,oi.num,oi.title,oi.price from shop_productOrders o " +
+            "left join shop_productOrderItems oi on oi.orderId=o.id where o.id=#{id}")
+    ProductOrderDetailModel getOrderById(@Param("id")long id);
 }
--- adminserver/src/main/java/com/zwdbj/server/adminserver/service/shop/service/productOrder/service/ProductOrderService.java
+++ adminserver/src/main/java/com/zwdbj/server/adminserver/service/shop/service/productOrder/service/ProductOrderService.java
@@ -1,7 +1,57 @@
 package com.zwdbj.server.adminserver.service.shop.service.productOrder.service;
 
+import com.zwdbj.server.adminserver.service.shop.service.productOrder.mapper.IProductOrderMapper;
+import com.zwdbj.server.adminserver.service.shop.service.productOrder.model.ProductOrderDetailModel;
+import com.zwdbj.server.adminserver.service.shop.service.productOrder.model.ProductOrderInput;
+import com.zwdbj.server.adminserver.service.shop.service.productOrder.model.ProductOrderModel;
+import com.zwdbj.server.adminserver.service.shop.service.receiveAddress.model.ReceiveAddressModel;
+import com.zwdbj.server.adminserver.service.shop.service.receiveAddress.service.ReceiveAddressService;
+import com.zwdbj.server.adminserver.service.user.service.UserService;
+import com.zwdbj.server.utility.model.ServiceStatusInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductOrderService {
+
+    @Autowired
+    IProductOrderMapper productOrderMapper;
+    @Autowired
+    UserService userService;
+    @Autowired
+    ReceiveAddressService receiveAddressServiceImpl;
+
+    public List<ProductOrderModel> getStoreOrders(long storeId, ProductOrderInput input){
+        try {
+            List<ProductOrderModel> orderModels = this.productOrderMapper.getStoreOrders(storeId,input);
+            for (ProductOrderModel model:orderModels){
+                model.setNickName(this.userService.getUserDetail(model.getUserId()).getNickName());
+                ReceiveAddressModel addressModel = this.receiveAddressServiceImpl.getReceiveAddressById(model.getReceiveAddressId()).getData();
+                if (addressModel!=null)
+                    model.setAddressModel(addressModel);
+            }
+            return orderModels;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public ServiceStatusInfo<ProductOrderDetailModel> getOrderById(long orderId){
+        try {
+            ProductOrderDetailModel model = this.productOrderMapper.getOrderById(orderId);
+            ReceiveAddressModel addressModel = this.receiveAddressServiceImpl.getReceiveAddressById(model.getReceiveAddressId()).getData();
+            model.setNickName(this.userService.getUserDetail(model.getUserId()).getNickName());
+            model.setAddressModel(addressModel);
+            return new ServiceStatusInfo<>(0,"",model);
+        }catch (Exception e){
+            return new ServiceStatusInfo<>(1, "获得订单失败：" + e.getMessage(), null);
+        }
+
+
+
+    }
 }
