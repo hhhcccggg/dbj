f3fc65bb88a23bb279344405c1f5f7fabb911933
wangpeihan <wangpeihan@zwdbj.com>
2019-01-10 17:55:09 +0800

    api-shop-order 加鎖

--- mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/shop/order/service/OrderService.java
+++ mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/shop/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.zwdbj.server.mobileapi.service.shop.order.service;
 
+import com.ecwid.consul.v1.ConsulClient;
 import com.zwdbj.server.mobileapi.service.shop.order.mapper.IOrderMapper;
 import com.zwdbj.server.mobileapi.service.shop.order.model.AddNewOrderInput;
 import com.zwdbj.server.mobileapi.service.shop.order.model.ProductOrderDetailModel;
@@ -12,6 +13,7 @@ import com.zwdbj.server.mobileapi.service.wxMiniProgram.receiveAddress.model.Rec
 import com.zwdbj.server.mobileapi.service.wxMiniProgram.receiveAddress.service.ReceiveAddressService;
 import com.zwdbj.server.utility.common.UniqueIDCreater;
 import com.zwdbj.server.utility.common.shiro.JWTUtil;
+import com.zwdbj.server.utility.consulLock.unit.Lock;
 import com.zwdbj.server.utility.model.ServiceStatusInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -67,54 +69,63 @@ public class OrderService {
 
     @Transactional
     public ServiceStatusInfo<Integer> createOrder(AddNewOrderInput input){
+        String key = String.valueOf(input.getProductskuId());
+        ConsulClient consulClient = new ConsulClient("localhost", 8500);    // 创建与Consul的连接
+        Lock lock = new Lock(consulClient, "mobileapi", "productOrder:" + key);
         try {
-            // TODO 考虑加锁
-            long userId = JWTUtil.getCurrentId();
-            //如果有限购，则要查看订单表，看兑换次数是否已经用完
-            if (input.getLimitPerPerson()!=0){
-                int account = this.orderMapper.userBuyProductAccounts(userId,input.getProductId());
-                if (account>=input.getLimitPerPerson()){
-                    return new ServiceStatusInfo<>(1, "您只能购买此商品"+input.getLimitPerPerson()+"个", 0);
-                }else if ((account+input.getNum())>input.getLimitPerPerson()){
-                    return new ServiceStatusInfo<>(1, "您只能购买此商品"+input.getLimitPerPerson()+"个", 0);
+            if (lock.lock(true, 500L, 2)){
+                // TODO 考虑加锁
+                long userId = JWTUtil.getCurrentId();
+                //如果有限购，则要查看订单表，看兑换次数是否已经用完
+                if (input.getLimitPerPerson()!=0){
+                    int account = this.orderMapper.userBuyProductAccounts(userId,input.getProductId());
+                    if (account>=input.getLimitPerPerson()){
+                        return new ServiceStatusInfo<>(1, "您只能购买此商品"+input.getLimitPerPerson()+"个", 0);
+                    }else if ((account+input.getNum())>input.getLimitPerPerson()){
+                        return new ServiceStatusInfo<>(1, "您只能购买此商品"+input.getLimitPerPerson()+"个", 0);
+                    }
+
+                }
+                //查看商品的库存是否满足
+                boolean a = this.productServiceImpl.getProductInventory(input.getProductId(),input.getProductskuId(),input.getNum()).getData();
+                if (!a)return new ServiceStatusInfo<>(1, "商品库存不足", 0);
+                long orderId = UniqueIDCreater.generateID();
+                if (input.getUseCoin()!=0){
+                    //查看user的账户小饼干是否够支付订单费用
+                    this.userAssetServiceImpl.userIsExist(userId);
+                    //用户小饼干总数
+                    long counts = userAssetServiceImpl.getCoinsByUserId().getData();
+                    if (counts<0 || counts<input.getUseCoin()){
+                        return new ServiceStatusInfo<>(1, "您的小饼干不足，请获取足够的小饼干", 0);
+                    }
                 }
 
-            }
-            //查看商品的库存是否满足
-            boolean a = this.productServiceImpl.getProductInventory(input.getProductId(),input.getProductskuId(),input.getNum()).getData();
-            if (!a)return new ServiceStatusInfo<>(1, "商品库存不足", 0);
-            long orderId = UniqueIDCreater.generateID();
-            if (input.getUseCoin()!=0){
-                //查看user的账户小饼干是否够支付订单费用
-                this.userAssetServiceImpl.userIsExist(userId);
-                //用户小饼干总数
-                long counts = userAssetServiceImpl.getCoinsByUserId().getData();
-                if (counts<0 || counts<input.getUseCoin()){
-                    return new ServiceStatusInfo<>(1, "您的小饼干不足，请获取足够的小饼干", 0);
-                }
+
+                //创建order
+                int payment = input.getPrice()*input.getNum()+input.getDeliveryFee();
+                this.orderMapper.createOrder(orderId,userId,input,payment);
+                //创建OrderItem
+                long orderItemId = UniqueIDCreater.generateID();
+                int price = input.getPrice();
+                int totalFee = price*input.getNum();
+                this.orderMapper.createOrderItem(orderItemId,orderId,input,price,totalFee);
+                // 减去商品和sku的库存并更新销量
+                this.productServiceImpl.updateProductNum(input.getProductId(),input.getProductskuId(),input.getNum());
+                //兑换后减去用户所需的小饼干
+                boolean flag = this.userAssetServiceImpl.minusUserCoins(input.getUseCoin(),userId,orderId);
+                if (!flag)return new ServiceStatusInfo<>(1,"下单失败",0);
+                // TODO 优惠券的使用
+                return new ServiceStatusInfo<>(0,"下单成功",1);
             }
 
-
-            //创建order
-            int payment = input.getPrice()*input.getNum()+input.getDeliveryFee();
-            this.orderMapper.createOrder(orderId,userId,input,payment);
-            //创建OrderItem
-            long orderItemId = UniqueIDCreater.generateID();
-            int price = input.getPrice();
-            int totalFee = price*input.getNum();
-            this.orderMapper.createOrderItem(orderItemId,orderId,input,price,totalFee);
-            // 减去商品和sku的库存并更新销量
-            this.productServiceImpl.updateProductNum(input.getProductId(),input.getProductskuId(),input.getNum());
-            //兑换后减去用户所需的小饼干
-            boolean flag = this.userAssetServiceImpl.minusUserCoins(input.getUseCoin(),userId,orderId);
-            if (!flag)return new ServiceStatusInfo<>(1,"下单失败",0);
-            // TODO 优惠券的使用
-            return new ServiceStatusInfo<>(0,"下单成功",1);
 
         }catch (Exception e){
             return new ServiceStatusInfo<>(1,"下单失败"+e.getMessage(),0);
+        }finally {
+            lock.unlock();
         }
 
+        return new ServiceStatusInfo<>(1,"下单失败",0);
     }
 
     @Transactional
