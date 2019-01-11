a57c9d71813162ae9606654ac179da2c43a608a1
hhhcccggg <18380212720@163.com>
2019-01-11 17:19:58 +0800

    debug

--- mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/shop/comments/mapper/ShopCommentsMapper.java
+++ mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/shop/comments/mapper/ShopCommentsMapper.java
@@ -20,7 +20,7 @@ public interface ShopCommentsMapper {
     @Select("SELECT u.id as userId ,u.userName,u.avatarUrl,c.contentTxt,c.heartCount,c.createTime,d.name as resourceName," +
             "c.resourceOwnerId,c.resourceTypeId,c.rate,ce.commentId,ce.type,ce.dataId,ce.dataContent " +
             "FROM core_comments as c,core_comment_extraDatas as ce,core_users as u,shop_discountCoupons as d " +
-            "WHERE c.resourceOwnerId =d.id and c.userId=u.id and ce.commentId=c.id and d.storeId=1 and c.resourceTypeId=1 order by c.createTime")
+            "WHERE c.resourceTypeId=1,d.storeId=#{storeId} and c.resourceOwnerId =d.id and c.userId=u.id and ce.commentId=c.id and c.isOwner=1 order by c.createTime")
     List<ShopCommentsExtraDatas> commentList(@Param("storeId") long storeId);
 
     //用户发布评论
