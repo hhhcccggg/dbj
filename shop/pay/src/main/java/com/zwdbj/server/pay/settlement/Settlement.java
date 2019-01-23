49c418682b77c9c6feb0ac68de38f93a5594aa36
wangpeihan <wangpeihan@zwdbj.com>
2019-01-23 13:07:56 +0800

    结算

--- shop/pay/src/main/java/com/zwdbj/server/pay/settlement/Settlement.java
+++ shop/pay/src/main/java/com/zwdbj/server/pay/settlement/Settlement.java
@@ -4,43 +4,30 @@ import com.zwdbj.server.pay.settlement.protocol.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class Settlement implements ISettlement {
-    @Autowired
-    private IUseCoupon useCoupon;
-    @Autowired
-    private IUseCoins useCoins;
     @Override
     public SettlementResult settlement(int amount, long orderId) {
-        return this.settlement(amount,orderId,useCoins,useCoupon);
+        return this.settlement(amount,orderId,0,null);
     }
 
     @Override
-    public SettlementResult settlement(int amount, long orderId, IUseCoins useCoins, IUseCoupon useCoupon) {
+    public SettlementResult settlement(int amount, long orderId, int coins, Coupon coupon) {
         SettlementResult result = new SettlementResult();
         result.setOrderId(orderId);
         result.setTotalAmount(amount);
         result.setCouponId(0);
-        this.useCoins = useCoins;
-        this.useCoupon = useCoupon;
         int useCoinsAmount = 0;
-        if(this.useCoins!=null) {
-            int coins = this.useCoins.coins();
-            if (coins <= 0) {
-                useCoinsAmount = 0;
-            } else if (coins % 10 != 0) {
-                useCoinsAmount = 0;
-            } else {
-                useCoinsAmount = coins / 10 * 100;
-            }
+        if (coins <= 0) {
+            useCoinsAmount = 0;
+        } else if (coins % 10 != 0) {
+            useCoinsAmount = 0;
+        } else {
+            useCoinsAmount = coins / 10 * 100;
         }
         //先抵扣，然后打折
         result.setUsedCoins(useCoinsAmount);
         int remainAmount = amount - useCoinsAmount;
         result.setActualAmount(remainAmount);
         //处理优惠券
-        Coupon coupon = null;
-        if (this.useCoupon!=null) {
-            coupon = this.useCoupon.coupons();
-        }
         if (coupon!=null&&coupon.getLimitMoney()>=amount) {
             if (coupon.getDiscountType().equals("CASH")) {
                 result.setCouponId(coupon.getId());
--- shop/pay/src/main/java/com/zwdbj/server/pay/settlement/protocol/ISettlement.java
+++ shop/pay/src/main/java/com/zwdbj/server/pay/settlement/protocol/ISettlement.java
@@ -16,11 +16,11 @@ public interface ISettlement {
      * 结算
      * @param amount 原总金额，单位分
      * @param orderId 订单编号
-     * @param useCoins 金币数据协议
-     * @param useCoupon 优惠券数据协议
+     * @param coins 金币数据协议
+     * @param coupon 优惠券数据协议
      * @return 结算结果
      */
     SettlementResult settlement(int amount,long orderId,
-                    IUseCoins useCoins,
-                    IUseCoupon useCoupon);
+                    int coins,
+                    Coupon coupon);
 }
