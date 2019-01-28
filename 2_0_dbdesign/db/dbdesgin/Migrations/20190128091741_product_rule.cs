2451d6be5d33515fe6e082789dec07cecbe94769
limingru <limingru@zwdbj.com>
2019-01-28 17:29:02 +0800

    修改产品表 增加规则字段

---
+++ 2_0_dbdesign/db/dbdesgin/Migrations/20190128091741_product_rule.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dbdesgin.Migrations
+{
+    public partial class product_rule : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ruleDescription",
+                table: "shop_products",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ruleDescription",
+                table: "shop_products");
+        }
+    }
+}
---
+++ adminserver/src/main/resources/db/migration/V24__product_rule.sql
@@ -0,0 +1,5 @@
+﻿ALTER TABLE `shop_products` ADD `ruleDescription` longtext NULL;
+
+INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+VALUES ('20190128091741_product_rule', '2.1.4-rtm-31024');
+
