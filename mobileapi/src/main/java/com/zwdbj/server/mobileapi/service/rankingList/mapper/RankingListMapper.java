deadb4ddd4decde8bdd17bddbfebead9e70a6ac4
hhhcccggg <18380212720@163.com>
2019-01-30 10:20:09 +0800

    修改排行榜缓存时间

--- mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/rankingList/mapper/RankingListMapper.java
+++ mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/rankingList/mapper/RankingListMapper.java
@@ -11,18 +11,21 @@ import java.util.List;
 @Mapper
 public interface RankingListMapper {
 
-    @Select("SELECT u.id as userId,u.nickName,u.avatarUrl,ua.totalCoins,(SELECT COUNT(*) FROM core_userAssets WHERE totalCoins>=ua.totalCoins) as rank " +
-            "from core_userAssets as ua,core_users as u WHERE ua.userId=u.id ORDER BY ua.totalCoins DESC LIMIT 0,20")
+    @Select("SELECT u.id as userId,u.nickName,u.avatarUrl,ua.userId,ua.totalCoins ,@rownum:=@rownum+1 as rank" +
+            "            from(SELECT @rownum:=0) r, core_userAssets as ua ,core_users as u WHERE ua.userId=u.id ORDER BY ua.totalCoins DESC  LIMIT 0,20")
     List<RankingListInfo> searchTotalRank();
 
-    @Select("SELECT u.id as userId,u.nickName,u.avatarUrl,ua.totalCoins,(SELECT COUNT(*) FROM core_userAssets WHERE totalCoins>=ua.totalCoins )as rank " +
-            "from core_userAssets as ua,core_users as u WHERE ua.userId=u.id and u.id=#{userId}")
+    @Select("SELECT u.id as userId,u.nickName,u.avatarUrl,t.totalCoins,t.rank FROM " +
+            "(SELECT ua.userId,ua.totalCoins ,@rownum:=@rownum+1 as rank" +
+            "            from(SELECT @rownum:=0) r, core_userAssets as ua ORDER BY ua.totalCoins DESC ) as t" +
+            ",core_users as u WHERE u.id=t.userId AND t.userId=#{userId}")
     RankingListInfo searchByUser(@Param("userId") long userId);
 
-    @Select("SELECT u.id as userId,u.nickName,u.avatarUrl,ua.totalCoins ,(SELECT COUNT(*) FROM core_userAssets WHERE totalCoins>=ua.totalCoins and " +
-            "userId in (SELECT userId FROM core_followers WHERE followerUserId=#{userId})) as rank " +
-            "from core_userAssets as ua,core_users as u  WHERE ua.userId=u.id and u.id in(SELECT userId FROM core_followers WHERE followerUserId=#{userId}) " +
-            "ORDER BY  ua.totalCoins DESC,u.createTime")
+    @Select("SELECT u.id as userId,u.nickName,u.avatarUrl,t.totalCoins,@rownum:=@rownum+1 as rank FROM (SELECT @rownum:=0) r," +
+            "(SELECT ua.userId,ua.totalCoins " +
+            "            from core_userAssets as ua,(SELECT userId FROM core_followers WHERE followerUserId=#{userId} ) as f WHERE ua.userId =f.userId  ORDER BY ua.totalCoins DESC " +
+            ") as t" +
+            ",core_users as u WHERE u.id=t.userId ")
     List<RankingListInfo> searchFriendRank(@Param("userId") long userId);
 
     @Select("select u.id as userId,u.nickName,u.avatarUrl from core_users as u " +
