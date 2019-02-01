68eec720b97ceea47a6e28feac025713d9dc228b
wangpeihan <wangpeihan@zwdbj.com>
2019-02-01 16:03:16 +0800

    评论

--- mobileapi/src/main/java/com/zwdbj/server/mobileapi/controller/CommentController.java
+++ mobileapi/src/main/java/com/zwdbj/server/mobileapi/controller/CommentController.java
@@ -35,15 +35,15 @@ public class CommentController {
     })
     public ResponsePageInfoData<List<CommentInfoDto>> list(@PathVariable long resId, @RequestParam(value = "pageNo", required = true, defaultValue = "1") int pageNo,
                                                            @RequestParam(value = "rows", required = true, defaultValue = "30") int rows) {
+        Long commentNum = this.commentService.findCommentNumById(resId);
         Page<CommentInfoDto> pageInfo = PageHelper.startPage(pageNo, rows);
-        List<CommentInfoDto> comments = commentService.list(resId, pageNo);
+        List<CommentInfoDto> comments = commentService.list(resId, pageNo,rows);
         //过滤违规评论
         for (CommentInfoDto dto : comments) {
             if (dto.getReviewStatus() != null && (dto.getReviewStatus().equals("reviewing") || dto.getReviewStatus().equals("review")) && dto.getUserId() != JWTUtil.getCurrentId()) {
                 dto.setContentTxt("评论审核中...");
             }
         }
-        Long commentNum = this.commentService.findCommentNumById(resId);
         return new ResponsePageInfoData<>(ResponseDataCode.STATUS_NORMAL, "", comments, commentNum);
     }
 
