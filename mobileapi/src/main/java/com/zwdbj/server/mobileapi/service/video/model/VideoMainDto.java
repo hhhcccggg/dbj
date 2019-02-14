5b79f7c249da7a8c5f11e280f1d38fb7c038dfc0
wuyanjiang <616352178@qq.com>
2019-02-14 17:58:38 +0800

    修改返回对象

--- mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/video/model/VideoMainDto.java
+++ mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/video/model/VideoMainDto.java
@@ -31,23 +31,63 @@ public class VideoMainDto {
     }
 
     class VideoMain{
-       private long id;
-       @ApiModelProperty(value = "视频路径")
-       private String videoUrl;
-       @ApiModelProperty(value = "标题")
-       private String title;
-       @ApiModelProperty(value = "封面图片")
-       private String coverImageUrl;
-       @ApiModelProperty(value = "标签")
-       private String tags;
-       @ApiModelProperty(value = "头像")
-       private String avatarUrl;
-       @ApiModelProperty(value = "昵称")
-       private String username;
-       @ApiModelProperty(value = "点赞数")
-       private long heartCount;
+        private long id;
+        @ApiModelProperty(value = "视频路径")
+        private String videoUrl;
+        @ApiModelProperty(value = "标题")
+        private String title;
+        @ApiModelProperty(value = "封面图片")
+        private String coverImageUrl;
+        @ApiModelProperty(value = "封面图片宽")
+        private double coverImageWidth;
+        @ApiModelProperty(value = "封面图片高")
+        private double coverImageHeight;
+        @ApiModelProperty(value = "标签")
+        private String tags;
+        @ApiModelProperty(value = "头像")
+        private String avatarUrl;
+        @ApiModelProperty(value = "昵称")
+        private String username;
+        @ApiModelProperty(value = "点赞数")
+        private long heartCount;
+        @ApiModelProperty(value = "店铺名称")
+        private String storeName;
+        @ApiModelProperty(value = "logo地址")
+        private String logoUrl;
 
-       public String getAvatarUrl() {
+        public double getCoverImageWidth() {
+            return coverImageWidth;
+        }
+
+        public void setCoverImageWidth(double coverImageWidth) {
+            this.coverImageWidth = coverImageWidth;
+        }
+
+        public double getCoverImageHeight() {
+            return coverImageHeight;
+        }
+
+        public void setCoverImageHeight(double coverImageHeight) {
+            this.coverImageHeight = coverImageHeight;
+        }
+
+        public String getStoreName() {
+            return storeName;
+        }
+
+        public void setStoreName(String storeName) {
+            this.storeName = storeName;
+        }
+
+        public String getLogoUrl() {
+            return logoUrl;
+        }
+
+        public void setLogoUrl(String logoUrl) {
+            this.logoUrl = logoUrl;
+        }
+
+        public String getAvatarUrl() {
            return avatarUrl;
        }
 
