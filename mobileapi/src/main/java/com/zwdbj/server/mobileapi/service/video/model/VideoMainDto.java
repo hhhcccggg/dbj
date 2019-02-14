b10e5fd56655372b2d9ff60bcf7107a03349a00b
wuyanjiang <616352178@qq.com>
2019-02-14 14:44:37 +0800

    修改主页传入传出参数

--- mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/video/model/VideoMainDto.java
+++ mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/video/model/VideoMainDto.java
@@ -3,86 +3,112 @@ package com.zwdbj.server.mobileapi.service.video.model;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import java.util.List;
+
 @ApiModel(description = "首页视频返回对象")
 public class VideoMainDto {
 
-    private long id;
-    @ApiModelProperty(value = "视频路径")
-    private String videoUrl;
-    @ApiModelProperty(value = "标题")
-    private String title;
-    @ApiModelProperty(value = "封面图片")
-    private String coverImageUrl;
-    @ApiModelProperty(value = "标签")
-    private String tags;
-    @ApiModelProperty(value = "头像")
-    private String avatarUrl;
-    @ApiModelProperty(value = "昵称")
-    private String username;
-    @ApiModelProperty(value = "点赞数")
-    private long heartCount;
+    @ApiModelProperty(value = "分页字段")
+    String scroll_id;
 
-    public String getAvatarUrl() {
-        return avatarUrl;
+    @ApiModelProperty(value = "视频数据")
+    List<VideoMain> videoMains;
+
+    public String getScroll_id() {
+        return scroll_id;
     }
 
-    public void setAvatarUrl(String avatarUrl) {
-        this.avatarUrl = avatarUrl;
+    public void setScroll_id(String scroll_id) {
+        this.scroll_id = scroll_id;
     }
 
-    public String getUsername() {
-        return username;
+    public List<VideoMain> getVideoMains() {
+        return videoMains;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setVideoMains(List<VideoMain> videoMains) {
+        this.videoMains = videoMains;
     }
 
-    public long getHeartCount() {
-        return heartCount;
-    }
+    class VideoMain{
+       private long id;
+       @ApiModelProperty(value = "视频路径")
+       private String videoUrl;
+       @ApiModelProperty(value = "标题")
+       private String title;
+       @ApiModelProperty(value = "封面图片")
+       private String coverImageUrl;
+       @ApiModelProperty(value = "标签")
+       private String tags;
+       @ApiModelProperty(value = "头像")
+       private String avatarUrl;
+       @ApiModelProperty(value = "昵称")
+       private String username;
+       @ApiModelProperty(value = "点赞数")
+       private long heartCount;
 
-    public void setHeartCount(long heartCount) {
-        this.heartCount = heartCount;
-    }
+       public String getAvatarUrl() {
+           return avatarUrl;
+       }
 
-    public long getId() {
-        return id;
-    }
+       public void setAvatarUrl(String avatarUrl) {
+           this.avatarUrl = avatarUrl;
+       }
 
-    public void setId(long id) {
-        this.id = id;
-    }
+       public String getUsername() {
+           return username;
+       }
 
-    public String getVideoUrl() {
-        return videoUrl;
-    }
+       public void setUsername(String username) {
+           this.username = username;
+       }
 
-    public void setVideoUrl(String videoUrl) {
-        this.videoUrl = videoUrl;
-    }
+       public long getHeartCount() {
+           return heartCount;
+       }
 
-    public String getTitle() {
-        return title;
-    }
+       public void setHeartCount(long heartCount) {
+           this.heartCount = heartCount;
+       }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+       public long getId() {
+           return id;
+       }
 
-    public String getCoverImageUrl() {
-        return coverImageUrl;
-    }
+       public void setId(long id) {
+           this.id = id;
+       }
 
-    public void setCoverImageUrl(String coverImageUrl) {
-        this.coverImageUrl = coverImageUrl;
-    }
+       public String getVideoUrl() {
+           return videoUrl;
+       }
 
-    public String getTags() {
-        return tags;
-    }
+       public void setVideoUrl(String videoUrl) {
+           this.videoUrl = videoUrl;
+       }
 
-    public void setTags(String tags) {
-        this.tags = tags;
-    }
+       public String getTitle() {
+           return title;
+       }
+
+       public void setTitle(String title) {
+           this.title = title;
+       }
+
+       public String getCoverImageUrl() {
+           return coverImageUrl;
+       }
+
+       public void setCoverImageUrl(String coverImageUrl) {
+           this.coverImageUrl = coverImageUrl;
+       }
+
+       public String getTags() {
+           return tags;
+       }
+
+       public void setTags(String tags) {
+           this.tags = tags;
+       }
+   }
 }
