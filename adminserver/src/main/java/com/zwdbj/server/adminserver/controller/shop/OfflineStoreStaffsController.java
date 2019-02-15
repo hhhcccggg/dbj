fa39e63289e014443e761d06eaf9ca243521b948
hhhcccggg <18380212720@163.com>
2019-02-15 18:01:24 +0800

    商家搜索

--- adminserver/src/main/java/com/zwdbj/server/adminserver/controller/shop/OfflineStoreStaffsController.java
+++ adminserver/src/main/java/com/zwdbj/server/adminserver/controller/shop/OfflineStoreStaffsController.java
@@ -1,19 +1,14 @@
 package com.zwdbj.server.adminserver.controller.shop;
 
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
 import com.zwdbj.server.adminserver.service.shop.service.offlineStoreStaffs.model.OfflineStoreStaffs;
 import com.zwdbj.server.adminserver.service.shop.service.offlineStoreStaffs.service.OfflineStoreStaffsService;
 import com.zwdbj.server.utility.model.ResponseData;
 import com.zwdbj.server.utility.model.ResponseDataCode;
-import com.zwdbj.server.utility.model.ResponsePageInfoData;
 import com.zwdbj.server.utility.model.ServiceStatusInfo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/shop/offlineStoreStaffs/dbj")
@@ -55,25 +50,25 @@ public class OfflineStoreStaffsController {
         return new ResponseData<>(ResponseDataCode.STATUS_ERROR, serviceStatusInfo.getMsg(), null);
     }
 
-    @RequestMapping(value = "/select", method = RequestMethod.GET)
-    @ApiOperation(value = "查询所有门店员工，代言人")
-    public ResponsePageInfoData<List<OfflineStoreStaffs>> select(@RequestParam(value = "pageNo", defaultValue = "1", required = true) int pageNo,
-                                                                 @RequestParam(value = "rows", defaultValue = "30", required = true) int rows) {
-        PageHelper.startPage(pageNo, rows);
-        List<OfflineStoreStaffs> list = offlineStoreStaffsServiceImpl.select().getData();
-        PageInfo<OfflineStoreStaffs> pageInfo = new PageInfo<>(list);
-        return new ResponsePageInfoData<>(ResponseDataCode.STATUS_NORMAL, "", list, pageInfo.getTotal());
-    }
-
-    @RequestMapping(value = "/select/{id}", method = RequestMethod.GET)
-    @ApiOperation(value = "通过Id查询线下门店服务范围")
-    public ResponseData<OfflineStoreStaffs> selectById(@PathVariable("id") Long id) {
-
-        ServiceStatusInfo<OfflineStoreStaffs> serviceStatusInfo = offlineStoreStaffsServiceImpl.selectById(id);
-        if (serviceStatusInfo.isSuccess()) {
-            return new ResponseData(ResponseDataCode.STATUS_NORMAL, "", serviceStatusInfo.getData());
-        }
-        return new ResponseData(ResponseDataCode.STATUS_ERROR, serviceStatusInfo.getMsg(), null);
-
-    }
+//    @RequestMapping(value = "/select", method = RequestMethod.GET)
+//    @ApiOperation(value = "查询所有门店员工，代言人")
+//    public ResponsePageInfoData<List<OfflineStoreStaffs>> select(@RequestParam(value = "pageNo", defaultValue = "1", required = true) int pageNo,
+//                                                                 @RequestParam(value = "rows", defaultValue = "30", required = true) int rows) {
+//        PageHelper.startPage(pageNo, rows);
+//        List<OfflineStoreStaffs> list = offlineStoreStaffsServiceImpl.select().getData();
+//        PageInfo<OfflineStoreStaffs> pageInfo = new PageInfo<>(list);
+//        return new ResponsePageInfoData<>(ResponseDataCode.STATUS_NORMAL, "", list, pageInfo.getTotal());
+//    }
+//
+//    @RequestMapping(value = "/select/{id}", method = RequestMethod.GET)
+//    @ApiOperation(value = "通过Id查询线下门店服务范围")
+//    public ResponseData<OfflineStoreStaffs> selectById(@PathVariable("id") Long id) {
+//
+//        ServiceStatusInfo<OfflineStoreStaffs> serviceStatusInfo = offlineStoreStaffsServiceImpl.selectById(id);
+//        if (serviceStatusInfo.isSuccess()) {
+//            return new ResponseData(ResponseDataCode.STATUS_NORMAL, "", serviceStatusInfo.getData());
+//        }
+//        return new ResponseData(ResponseDataCode.STATUS_ERROR, serviceStatusInfo.getMsg(), null);
+//
+//    }
 }
