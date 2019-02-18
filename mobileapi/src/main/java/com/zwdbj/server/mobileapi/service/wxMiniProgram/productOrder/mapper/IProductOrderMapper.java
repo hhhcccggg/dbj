174e14ee48b6a671db0a2a7a0641c24e37b6dc07
wuyanjiang <616352178@qq.com>
2019-02-18 09:47:55 +0800

    我的兑换字段查询修改

--- mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/wxMiniProgram/productOrder/mapper/IProductOrderMapper.java
+++ mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/wxMiniProgram/productOrder/mapper/IProductOrderMapper.java
@@ -41,7 +41,7 @@ public interface IProductOrderMapper {
     @Select("SELECT " +
             "oi.productId,r.receiverName,r.receiverPhone, r.receiverMobile,r.reveiverState,r.receiverCity, " +
             "r.receiverCountry,r.receiverAddress,p.imageUrls,oi.price, " +
-            "o.payment,o.`status`,o.endTime,o.actualPayment,oi.title,o.createTime,o.orderNo " +
+            "o.payment,o.`status`,o.endTime,o.actualPayment,oi.title,o.createTime,o.orderNo,r.receiverStreet,oi.productskuId " +
             "FROM  shop_productOrderItems oi " +
             "LEFT JOIN shop_productOrders o ON o.id = oi.orderId " +
             "LEFT JOIN shop_products p ON oi.productId=p.id " +
