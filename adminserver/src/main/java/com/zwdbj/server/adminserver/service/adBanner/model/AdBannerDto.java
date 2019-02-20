25f7159d4ed6224d84cf31a7ef00cdaabfb9e69d
wangpeihan <wangpeihan@zwdbj.com>
2019-02-20 10:43:49 +0800

    banner

--- adminserver/src/main/java/com/zwdbj/server/adminserver/service/adBanner/model/AdBannerDto.java
+++ adminserver/src/main/java/com/zwdbj/server/adminserver/service/adBanner/model/AdBannerDto.java
@@ -6,25 +6,74 @@ import io.swagger.annotations.ApiModelProperty;
 @ApiModel(description = "创建,修改adBanner")
 public class AdBannerDto {
     @ApiModelProperty(value = "名称")
-    String title;
+    private String title;
     @ApiModelProperty(value = "图片地址")
-    String imageUrl;
+    private String imageUrl;
     @ApiModelProperty(value = "关联的h5网页")
-    String refUrl;
+    private String refUrl;
     @ApiModelProperty(value = "不同地方的banner MINIAPP_HOME:微信小程序首页 APP_O2O_HOME:app周边首页 DISCOUNT_APP:app优惠折扣 COIN_TASK_APP:app金币任务")
-    Type type;
+    private String type;
     @ApiModelProperty(value = "平台类型 IOS:苹果ANDROID:安卓ALL:所有")
-    Platform platform;
+    private String platform;
     @ApiModelProperty(value = "状态 ONLINE:上线 OFFLINE:下线")
-    State state;
+    private State state;
+    @ApiModelProperty("banner活动的开始时间")
+    private String startTime;
+    @ApiModelProperty("banner活动的结束时间")
+    private String endTime;
+    @ApiModelProperty(value = "banner的展示范围")
+    private String exhibitionScope;
+    @ApiModelProperty(value = "此banner适用对象, 全部:ALL  新用户:NEWUSER    老用户:OLDUSER")
+    private String applyUser;
 
-    public Type getType() {
+    public String getType() {
         return type;
     }
 
-    public void setType(Type type) {
+    public void setType(String type) {
         this.type = type;
     }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getExhibitionScope() {
+        return exhibitionScope;
+    }
+
+    public void setExhibitionScope(String exhibitionScope) {
+        this.exhibitionScope = exhibitionScope;
+    }
+
+    public String getApplyUser() {
+        return applyUser;
+    }
+
+    public void setApplyUser(String applyUser) {
+        this.applyUser = applyUser;
+    }
+
 
     public String getTitle() {
         return title;
@@ -50,13 +99,6 @@ public class AdBannerDto {
         this.refUrl = refUrl;
     }
 
-    public Platform getPlatform() {
-        return platform;
-    }
-
-    public void setPlatform(Platform platform) {
-        this.platform = platform;
-    }
 
     public State getState() {
         return state;
