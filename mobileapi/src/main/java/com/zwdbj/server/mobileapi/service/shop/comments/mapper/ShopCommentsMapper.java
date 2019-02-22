65507d0b2e65d44c7d88b03e5e33da46fe8d3797
hhhcccggg <18380212720@163.com>
2019-02-22 09:51:12 +0800

    商家首页接口

--- mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/shop/comments/mapper/ShopCommentsMapper.java
+++ mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/shop/comments/mapper/ShopCommentsMapper.java
@@ -17,10 +17,10 @@ public interface ShopCommentsMapper {
             "where c.resourceTypeId=1 and c.reviewStatus='pass' and c.resourceOwnerId in(select id from shop_discountCoupons where storeId=#{storeId} )")
     UserComments CountComments(@Param("storeId") long storeId);
 
-    @Select("SELECT u.id as userId ,u.userName,u.avatarUrl,c.contentTxt,c.heartCount,c.createTime,d.name as resourceName," +
+    @Select("SELECT u.id as userId ,u.userName,u.avatarUrl,c.contentTxt,c.heartCount,c.createTime,p.name as resourceName," +
             "c.resourceOwnerId,c.resourceTypeId,c.rate,ce.commentId,ce.type,ce.dataId,ce.dataContent " +
-            "FROM core_comments as c,core_comment_extraDatas as ce,core_users as u,shop_discountCoupons as d " +
-            "WHERE c.resourceTypeId=1,d.storeId=#{storeId} and c.resourceOwnerId =d.id and c.userId=u.id and ce.commentId=c.id and c.isOwner=1 order by c.createTime")
+            "FROM core_comments as c,core_comment_extraDatas as ce,core_users as u,shop_products as p " +
+            "WHERE c.resourceTypeId=1 or c.resourceTypeId=2,p.storeId=#{storeId} and c.resourceOwnerId =p.id and c.userId=u.id and ce.commentId=c.id and c.isOwner=1 order by c.createTime")
     List<ShopCommentsExtraDatas> commentList(@Param("storeId") long storeId);
 
     //用户发布评论
