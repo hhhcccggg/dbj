2d516c0bf4de7ee6a510d524b0a6e70796c2acad
wuyanjiang <616352178@qq.com>
2019-02-22 15:05:16 +0800

    后台加上省市区接口

---
+++ adminserver/src/main/java/com/zwdbj/server/adminserver/service/cities/mapper/ICitiesMapper.java
@@ -0,0 +1,16 @@
+package com.zwdbj.server.adminserver.service.cities.mapper;
+
+import com.zwdbj.server.adminserver.service.cities.model.Cities;
+import com.zwdbj.server.adminserver.service.cities.model.LevelType;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface ICitiesMapper {
+
+    @Select("select * from core_cities where level = #{levelType} and parentId=#{parenId}")
+    List<Cities> selectCondition(@Param("levelType") LevelType levelType, @Param("parenId") long parenId);
+}
