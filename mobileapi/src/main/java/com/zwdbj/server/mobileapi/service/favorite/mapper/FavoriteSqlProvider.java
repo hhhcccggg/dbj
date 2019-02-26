0652c26f6181d5eacbea3ba10f34dd411df814e1
wuyanjiang <616352178@qq.com>
2019-02-26 11:38:16 +0800

    收藏查询修改

--- mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/favorite/mapper/FavoriteSqlProvider.java
+++ mobileapi/src/main/java/com/zwdbj/server/mobileapi/service/favorite/mapper/FavoriteSqlProvider.java
@@ -9,14 +9,18 @@ public class FavoriteSqlProvider {
 
     public String searchFavorite(Map map){
         SearchFavorite searchFavorite = (SearchFavorite) map.get("searchFavorite");
-        SQL sql = new SQL().SELECT("id","targetId","targetType","title","imageUrl","price");
-        sql.FROM("shop_Favorites");
+        SQL sql = new SQL().SELECT("id","targetId","targetType","title","imageUrl","price",
+                "(select ifnull(count(0),0) from shop_Favorites where shop_Favorites.isDeleted=0 and  shop_Favorites.targetType=sf.targetType and shop_Favorites.targetId = sf.targetId) as amount",
+                "(select GROUP_CONCAT(`name`) from core_categories where id in " +
+                        "(select serviceScopeId from o2o_offlineStoreServiceScopes where storeId=2 and isDeleted=0)  and isDeleted=0) as scopeServices");
+        sql.FROM("shop_Favorites as sf");
         if(searchFavorite.getTargetType() != null){
             sql.WHERE("targetType=#{searchFavorite.targetType}") ;
         }
         if(searchFavorite.getUserId()>0){
             sql.WHERE("userId=#{searchFavorite.userId}") ;
         }
+        sql.WHERE("isDeleted=0");
         sql.ORDER_BY("createTime desc");
         return sql.toString();
     }
