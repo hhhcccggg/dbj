958bb7e5f8d5f416bfb3a1cc16e5dd13efda75b6
limingru <limingru@zwdbj.com>
2019-02-27 15:30:16 +0800

    增加配置中心服务

---
+++ microservice/basearc/src/main/java/com/zwdbj/server/basearc/BasearcApplication.java
@@ -0,0 +1,13 @@
+package com.zwdbj.server.basearc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasearcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasearcApplication.class, args);
+    }
+
+}
