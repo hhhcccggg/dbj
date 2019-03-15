d67d13c433aad00d8a8eabedfb93d79994e012f7
lijiatai <lijiatai@zwdbj.com>
2019-03-15 16:46:33 +0800

    25656

--- adminserver/src/main/java/com/zwdbj/server/adminserver/service/shop/service/customerComments/mapper/CustomerCommentsSqlProvider.java
+++ adminserver/src/main/java/com/zwdbj/server/adminserver/service/shop/service/customerComments/mapper/CustomerCommentsSqlProvider.java
@@ -12,21 +12,20 @@ public class CustomerCommentsSqlProvider {
         SQL sql = new SQL()
                 .SELECT("c.id,c.contentTxt,c.rate,c.heartCount,u.username,u.id as userId," +
                         "c.createTime,c.resourceOwnerId,ce.type,ce.dataId,ce.dataContent,v.coverImageUrl")
-                .FROM("core_comments as c")
+                .FROM("core_comments c")
                 .LEFT_OUTER_JOIN("core_comment_extraDatas ce ON ce.commentId=c.id ")
                 .LEFT_OUTER_JOIN("core_users u ON u.id=c.userId")
                 .LEFT_OUTER_JOIN("core_videos v ON v.id=ce.dataId")
-                .WHERE("c.resourceOwnerId in (select p.id from shop_products as p," +
-                        "shop_stores as s where p.storeId=s.id and s.legalSubjectId="+legalSubjectId+")")
+                .WHERE("c.resourceOwnerId in (select p.id from shop_products p," +
+                        "shop_stores s where p.storeId=s.id and s.legalSubjectId="+legalSubjectId+")")
                 .WHERE("c.reviewStatus='pass'")
                 .WHERE("c.isOwner=1")
                 .WHERE("c.resourceTypeId=1");
         if (searchInfo.getUsername()!=null&&searchInfo.getUsername().length()>0) {
-            sql.WHERE(String.format("c.username like '%s'", ("%" + searchInfo.getUsername() + "%")));
+            sql.WHERE("u.username like '%"+searchInfo.getUsername()+"%'");
         }
-        if (searchInfo.getStartTime() !=null  && searchInfo.getStartTime().length()!=0
-                && searchInfo.getEndTime() !=null && searchInfo.getEndTime().length()!=0){
-            sql.WHERE(String.format("c.createTime between '%s' and '%s'",searchInfo.getStartTime(),searchInfo.getEndTime()));
+        if (searchInfo.getStartTime() !=null  && searchInfo.getStartTime().length()!=0&& searchInfo.getEndTime() !=null && searchInfo.getEndTime().length()!=0){
+            sql.WHERE("c.createTime between '"+searchInfo.getStartTime()+"' and '"+searchInfo.getEndTime()+"'");
         }
         sql.ORDER_BY("c.createTime desc");
         return sql.toString();
